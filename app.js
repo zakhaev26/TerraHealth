250bc7041524d16bac5a6a43f0cb92917e5f379a
Soubhik <b422056@iiit-bh.ac.in>
2023-04-11 21:31:57 +0530

    navbar and footer setup

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const bodyParser = require('body-parser')
-const express=require('express')
-const app=express()
-const prt=1080
-app.use(bodyParser.urlencoded({extended:true}))
+const express = require('express')
+const app = express()
+const prt = 1080
+app.use(bodyParser.urlencoded({ extended: true }))
 app.setMaxListeners(30);
 app.set("views", __dirname + '/views')
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'))
 
-app.get('/',(req,res)=>{
-    res.send('Hello');
-})
+app.get('/', (req, res) => {
+    res.render("homepg.ejs");
+}
+)
 
-app.listen(prt||process.env.PORT,()=>{
+app.listen(prt || process.env.PORT, () => {
     console.log(`App is running live on Port ${prt}`)
 })
