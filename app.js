7974724197ef4a5114cd3aa859e48a76d445523f
Soubhik <b422056@iiit-bh.ac.in>
2023-04-12 04:43:31 +0530

    logic fixed and pg added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const { builtinModules } = require('module')
 const app = express()
 const prt = 1080
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -7,21 +8,63 @@ app.setMaxListeners(30);
 app.set("views", __dirname + '/views')
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'))
-
+let bmi='';
+let suggestion='';
 app.get('/', (req, res) => {
-    res.render("homepg.ejs",{stylesheet:'/styles.css'});
+    res.render("homepg.ejs", { stylesheet: '/styles.css' });
 }
 )
 
-app.get('/about',(req,res)=>{
-    res.render('about.ejs',{stylesheet:'/styles.css'});
+app.get('/about', (req, res) => {
+    res.render('about.ejs', { stylesheet: '/styles.css' });
 })
 
-app.get('/bmi',(req,res)=>{
-    res.render('bmicalc.ejs',{stylesheet:'/stylesbmi.css'});
+app.get('/bmi', (req, res) => {
+    res.render('bmicalc.ejs', { stylesheet: '/stylesbmi.css' });
 })
 
+app.get('/bmiresult', (req, res) => {
+let ts=bmi
+console.log(ts);
+//learn about the chunk error:why only on passing the bmi it shows error?[FIND PATCH]
+res.render('bmiresult.ejs',{stylesheet:'/stylesbmi.css',bmi:bmi,suggestion:suggestion})
+}) 
 
+
+
+app.post('/bmicalc', (req, res) => {
+   const weight = Number(req.body.weight);
+    const height = Number(req.body.height);
+    const name = req.body.name;
+    bmi=(weight)/(height*height)
+    if(bmi<18.5){
+        suggestion=`Underweight (BMI < 18.5):If you are underweight, it's important to focus on gaining weight in a healthy way.
+        Try to eat more nutrient-dense foods such as whole grains, fruits, vegetables, and lean proteins.
+        Incorporate strength-training exercises into your fitness routine to build muscle mass.
+        Consult with a healthcare professional or registered dietitian to develop a safe and effective plan for gaining weight.`
+    }
+    else if(bmi>=18.5&&bmi <=24.9){
+        suggestion=`If you are in the normal BMI range, it's important to maintain your weight by eating a balanced diet and staying physically active.
+        Aim for a diet that includes a variety of nutrient-dense foods from all food groups.
+        Try to engage in regular physical activity such as brisk walking, running, or cycling.
+        Make sure to get enough sleep and manage stress levels, as these can impact weight and overall health.`
+    }
+    else if(bmi>=25&&bmi<=29.9){
+        suggestion=`If you are overweight, it's important to focus on losing weight in a healthy way to reduce the risk of obesity-related health problems.
+        Try to eat a balanced diet that is low in calories, fat, and sugar.
+        Engage in regular physical activity such as cardio exercises, strength-training exercises, or sports.
+        Consult with a healthcare professional or registered dietitian to develop a safe and effective plan for weight loss.`
+    }
+    else if(bmi>=30){
+        suggestion=`If you are obese, it's important to take steps to lose weight in a safe and effective way to reduce the risk of obesity-related health problems.
+        Try to eat a balanced diet that is low in calories, fat, and sugar.
+        Engage in regular physical activity such as cardio exercises, strength-training exercises, or sports.
+        Consider working with a healthcare professional or registered dietitian to develop a personalized plan for weight loss that may include medication or surgery`
+    }
+    // console.log(bmi)
+
+    res.redirect('/bmiresult');
+})
 
 app.listen(prt || process.env.PORT, () => {
     console.log(`App is running live on Port ${prt}`)
