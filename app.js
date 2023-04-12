5e9aba1008597d1acc0548cfd066c666096240ff
Soubhik <b422056@iiit-bh.ac.in>
2023-04-12 07:19:01 +0530

    bmr complete commit

--- app.js
+++ app.js
@@ -10,11 +10,17 @@ app.set("views", __dirname + '/views')
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'))
 let bmi='';
-let suggestion='';
-let imgsrc='';
-let errCode='';
-let currState='';
-let name='';
+let suggestionBMI='';
+let imgsrcBMI='';
+let errCodeBMI='';
+let currStateBMI='';
+let nameBMI='';
+let nameBMR='';
+let ageBMR='';
+let heightBMR='';
+let weightBMR='';
+let genderBMR='';
+let bmr='';
 app.get('/', (req, res) => {
     res.render("homepg.ejs", { stylesheet: '/styles.css' });
 }
@@ -33,10 +39,8 @@ app.get('/bmi', (req, res) => {
 })
 
 app.get('/bmiresult', (req, res) => {
-let ts=bmi
-console.log(ts);
 //learn about the chunk error:why only on passing the bmi it shows error?[FIND PATCH]
-res.render('bmiresult.ejs',{stylesheet:'/stylesbmi.css',name:name,bmi:bmi,suggestion:suggestion,imgsrc:imgsrc,currState:currState})
+res.render('bmiresult.ejs',{stylesheet:'/stylesbmi.css',name:nameBMI,bmi:bmi,suggestion:suggestionBMI,imgsrc:imgsrcBMI,currState:currStateBMI})
 }) 
 
 app.get('/bmr',(req,res)=>{
@@ -45,49 +49,71 @@ app.get('/bmr',(req,res)=>{
 
 
 app.post('/bmicalc', (req, res) => {
-   const weight = Number(req.body.weight);
-    const height = Number(req.body.height);
-    name = req.body.name;
-    bmi=(weight)/(height*height)
+   const weightBMI = Number(req.body.weight);
+    const heightBMI = Number(req.body.height);
+    nameBMI = req.body.name;
+    bmi=(weightBMI)/(heightBMI*heightBMI)
     if(bmi<18.5){
-        currState='Under Weight';
-        suggestion=`it's important to focus on gaining weight in a healthy way.
+        currStateBMI='Under Weight';
+        suggestionBMI=`it's important to focus on gaining weight in a healthy way.
 
         Try to eat more nutrient-dense foods such as whole grains, fruits, vegetables, and lean proteins.
         Incorporate strength-training exercises into your fitness routine to build muscle mass.
         Consult with a healthcare professional or registered dietitian to develop a safe and effective plan for gaining weight.`
-        imgsrc='underweight.jpg'
+        imgsrcBMI='underweight.jpg'
     }
     else if(bmi>=18.5&&bmi <=24.9){
-        currState='Normally Weight';
-        suggestion=`If you are in the normal BMI range, it's important to maintain your weight by eating a balanced diet and staying physically active.
+        currStateBMI='Normally Weight';
+        suggestionBMI=`If you are in the normal BMI range, it's important to maintain your weight by eating a balanced diet and staying physically active.
         Aim for a diet that includes a variety of nutrient-dense foods from all food groups.
         Try to engage in regular physical activity such as brisk walking, running, or cycling.
         Make sure to get enough sleep and manage stress levels, as these can impact weight and overall health.`
-        imgsrc='normal.jpg'
+        imgsrcBMI='normal.jpg'
     }
     else if(bmi>=25&&bmi<=29.9){
-        currState='Over Weight'
-        suggestion=`If you are overweight, it's important to focus on losing weight in a healthy way to reduce the risk of obesity-related health problems.
+        currStateBMI='Over Weight'
+        suggestionBMI=`If you are overweight, it's important to focus on losing weight in a healthy way to reduce the risk of obesity-related health problems.
         Try to eat a balanced diet that is low in calories, fat, and sugar.
         Engage in regular physical activity such as cardio exercises, strength-training exercises, or sports.
         Consult with a healthcare professional or registered dietitian to develop a safe and effective plan for weight loss.`
-        imgsrc='overweight.jpg'
+        imgsrcBMI='overweight.jpg'
     }
     else if(bmi>=30){
-        currState='Obese'
-        suggestion=`If you are obese, it's important to take steps to lose weight in a safe and effective way to reduce the risk of obesity-related health problems.
+        currStateBMI='Obese'
+        suggestionBMI=`If you are obese, it's important to take steps to lose weight in a safe and effective way to reduce the risk of obesity-related health problems.
         Try to eat a balanced diet that is low in calories, fat, and sugar.
         Engage in regular physical activity such as cardio exercises, strength-training exercises, or sports.
         Consider working with a healthcare professional or registered dietitian to develop a personalized plan for weight loss that may include medication or surgery`
-        imgsrc='obese.jpg'
+        imgsrcBMI='obese.jpg'
     }
     else{
         return res.redirect('/error');
     }
     // console.log(bmi)
-
     res.redirect('/bmiresult');
+})
+
+app.post('/bmrDET',(req,res)=>{
+    ageBMR=req.body.age;
+    nameBMR=req.body.name;
+    weightBMR=req.body.weight;
+    heightBMR=req.body.height;
+    genderBMR=req.body.gender;
+    //logic
+    if(genderBMR==='Male'){
+        bmr= 88.362 + (13.397*weightBMR) + (4.799*heightBMR)-(5.677*ageBMR)
+        console.log(bmr);
+        // Men: BMR = 88.362 + (13.397 x weight in kg) + (4.799 x height in cm) – (5.677 x age in years)
+    }
+    else if(genderBMR==='Female'){
+        bmr= 447.593 + (9.247*weightBMR) + (3.098*heightBMR)-(4.330*ageBMR)
+        // Women: BMR = 447.593 + (9.247 x weight in kg) + (3.098 x height in cm) – (4.330 x age in years)
+    }
+    res.redirect('/bmrRES')
+})
+
+app.get('/bmrRES',(req,res)=>{
+    res.render('bmrRES.ejs',{stylesheet:'/stylesbmi.css',name:nameBMR,bmr:bmr})
 })
 
 app.get('/:txt',(req,res)=>{
